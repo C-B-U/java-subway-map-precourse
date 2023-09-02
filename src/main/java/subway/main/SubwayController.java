15838253451bee1a78828fd821d5f105c58783fc
김연진 <love_homebody@naver.com>
2023-09-03 01:26:20 +0900

    feat(SubwayController): 메인 기능 선택시 해당 기능으로 연결

--- src/main/java/subway/main/SubwayController.java
+++ src/main/java/subway/main/SubwayController.java
@@ -1,5 +1,6 @@
 package subway.main;
 
+import subway.station.StationController;
 import subway.view.InputView;
 import subway.view.OutputView;
 
@@ -9,15 +10,24 @@ public class SubwayController {
 
     private final OutputView outputView;
     private final InputView inputView;
+    private final StationController stationController;
 
     public SubwayController(Scanner scanner) {
         this.outputView = new OutputView();
         this.inputView = new InputView(scanner);
+        this.stationController = new StationController(outputView, inputView);
     }
 
     public void start() {
         Initialization.init();
         outputView.printMainOptions();
         MainOption option = inputView.readOption();
+        link(option);
+    }
+
+    private void link(MainOption option) {
+        if (option.isStationManagement()) {
+            stationController.start();
+        }
     }
 }
