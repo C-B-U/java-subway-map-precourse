d7746ffe6805f8224f2fe17c2ac4a1052bd2f554
Jiyoon Kim <rlawldbs0911@naver.com>
2023-09-04 01:53:10 +0900

    feat(InputValidator): 입력받은 메인 기능 번호 유효성 검사하는 메서드 구현

--- src/main/java/subway/validator/Inputvalidator.java
+++ src/main/java/subway/validator/Inputvalidator.java
@@ -1,7 +1,13 @@
 package subway.validator;
 
-public class Inputvalidator {
-    public void validateIsDigit(String number) {
+import subway.domain.ErrorMessage;
 
+public class Inputvalidator {
+    private static final String REGEX = "^[0-9]+$";
+
+    public void validateIsDigit(String number) {
+        if(!number.matches(REGEX)) {
+            throw new IllegalArgumentException(ErrorMessage.WRONG_MAIN_FEATURE.toString());
+        }
     }
 }
