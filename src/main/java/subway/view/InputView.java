8c756e526d9bf42a330e6e2ee70c3ab10b32faca
Jiyoon Kim <rlawldbs0911@naver.com>
2023-09-04 07:13:21 +0900

    refactor(InputValidator): 입력값 유효성 검사 클래스 삭제 -> 비즈니스 로직에서 검증

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -3,19 +3,16 @@ package subway.view;
 import subway.domain.Station;
 import subway.domain.features.MainFeatures;
 import subway.domain.features.StationFeatures;
-import subway.validator.Inputvalidator;
 
 import java.util.Scanner;
 
 public class InputView {
-    private static final Inputvalidator inputValidator = new Inputvalidator();
     private static final OutputView outputView = new OutputView();
 
     public MainFeatures inputWantedFeature() {
         final Scanner scanner = new Scanner(System.in);
         String number = scanner.nextLine();
         try {
-            inputValidator.validateIsDigit(number);
             return MainFeatures.convert(number);
         } catch (IllegalArgumentException e) {
             outputView.printException(e);
@@ -27,7 +24,6 @@ public class InputView {
         final Scanner scanner = new Scanner(System.in);
         String number = scanner.nextLine();
         try {
-            inputValidator.validateIsDigit(number);
             return StationFeatures.convert(number);
         } catch (IllegalArgumentException e) {
             outputView.printException(e);
