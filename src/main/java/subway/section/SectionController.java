7d44170705412342a3d2d6ed63e74c680bc155f8
김연진 <love_homebody@naver.com>
2023-09-04 04:05:42 +0900

    feat(SectionOutputView): 구간 관리 화면 출력 기능 구현

--- src/main/java/subway/section/SectionController.java
+++ src/main/java/subway/section/SectionController.java
@@ -16,5 +16,6 @@ public class SectionController {
     }
 
     public void start() {
+        outputView.printSectionOption();
     }
 }
--- src/main/java/subway/view/section/SectionOutputView.java
+++ src/main/java/subway/view/section/SectionOutputView.java
@@ -1,4 +1,26 @@
 package subway.view.section;
 
+import subway.constant.OutputMessage;
+import subway.constant.PrintElement;
+import subway.section.SectionOption;
+import subway.station.StationOption;
+
+import java.util.Arrays;
+
 public class SectionOutputView {
+
+    public void printSectionOption() {
+        StringBuilder stringBuilder = new StringBuilder();
+        Arrays.stream(SectionOption.values())
+                .forEach(value -> appendValue(stringBuilder, value));
+        System.out.println(OutputMessage.STATION_OPTION_SELECT);
+        System.out.println(stringBuilder);
+    }
+
+    private void appendValue(StringBuilder stringBuilder, SectionOption option) {
+        stringBuilder.append(option.getOption())
+            .append(PrintElement.COMMA)
+            .append(option.getTitle())
+            .append(PrintElement.NEW_LINE);
+    }
 }
