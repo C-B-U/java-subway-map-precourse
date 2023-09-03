39be04c2e7c1a0670de2a18678dbd993462fd0e1
김연진 <love_homebody@naver.com>
2023-09-04 01:36:41 +0900

    feat(StioStations): 상행 종점역과 하행 종점역이 같을 경우 예외 발생

--- src/main/java/subway/line/StopStations.java
+++ src/main/java/subway/line/StopStations.java
@@ -1,15 +1,26 @@
 package subway.line;
 
+import subway.constant.ExceptionMessage;
 import subway.station.Station;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class StopStations {
 
     private final List<Station> element;
 
     public StopStations(List<Station> stations) {
+        validateDuplicate(stations);
         this.element = new ArrayList<>(stations);
+    }
+
+    private void validateDuplicate(List<Station> stations) {
+        Set<Station> checkStation = new HashSet<>(stations);
+        if (checkStation.size() != stations.size()) {
+            throw new IllegalArgumentException(ExceptionMessage.DUPLICATE_LAST_STATION.toString());
+        }
     }
 }
