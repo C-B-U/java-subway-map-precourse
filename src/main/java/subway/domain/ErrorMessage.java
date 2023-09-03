4abfd6abe2b4215167dd746ad437cdb61ec1a378
Jiyoon Kim <rlawldbs0911@naver.com>
2023-09-04 07:31:48 +0900

    feat(Line): 노선 비즈니스 유효성 검사 메서드 구현

--- src/main/java/subway/domain/ErrorMessage.java
+++ src/main/java/subway/domain/ErrorMessage.java
@@ -2,9 +2,10 @@ package subway.domain;
 
 public enum ErrorMessage {
     WRONG_STATION_NAME_LENGTH("지하철 역 이름은 2글자 이상만 가능합니다. 다시 입력해 주세요."),
+    WRONG_LINE_NAME_LENGTH("지하철 노선 이름은 2글자 이상만 가능합니다. 다시 입력해 주세요."),
     DUPLICATE_STATION("이미 등록된 역 이름입니다."),
     WRONG_FEATURE_NUMBER("선택할 수 없는 기능입니다. 다시 입력해 주세요."),
-    WRONG_STATION_NAME_COMPONENT("올바르지 않은 역 이름입니다. 다시 입력해 주세요."),
+    WRONG_NAME_COMPONENT("올바르지 않은 이름입니다. 다시 입력해 주세요."),
     NOT_EXIST_STATION("존재하지 않는 역입니다.");
 
     private static final String PREFIX = "[ERROR] ";
--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,10 +1,31 @@
 package subway.domain;
 
 public class Line {
-    private String name;
+    private static final String LINE_UNIT = "선";
+    private static final int MINIMUM_LENGTH = 2;
+
+    private final String name;
 
     public Line(String name) {
+        validateLinenName(name);
         this.name = name;
+    }
+
+    private void validateLinenName(String name) {
+        validateLinenNameLength(name);
+        validateLineNameComponent(name);
+    }
+
+    private void validateLinenNameLength(String name) {
+        if(!name.endsWith(LINE_UNIT)) {
+            throw new IllegalArgumentException(ErrorMessage.WRONG_NAME_COMPONENT.toString());
+        }
+    }
+
+    private void validateLineNameComponent(String name) {
+        if (name.length() < MINIMUM_LENGTH) {
+            throw new IllegalArgumentException(ErrorMessage.WRONG_LINE_NAME_LENGTH.toString());
+        }
     }
 
     public String getName() {
